30c9dbbec21f82850c2e158e136846212dd4895d
laetuz <laetuzg@gmail.com>
2023-10-13 13:52:57 +0700

    Initial Commit

---
+++ Recipetory/RecipetoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipetoryApp.swift
+//  Recipetory
+//
+//  Created by Ryo Martin on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipetoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
